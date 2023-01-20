9ea5819680cbb1d12e1d3d9fbfb1168ee1d61a58
marcelojsil <dev.marcelojs@gmail.com>
2023-01-19 22:27:14 -0300

    second commit

--- wp-content/themes/owlpress/sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! is_active_sidebar( 'owlpress-sidebar-primary' ) ) {	return; } ?>
-<div class="col-lg-4 col-md-12 col-12">
-	<div class="sidebar">
-		<?php dynamic_sidebar('owlpress-sidebar-primary'); ?>
-	</div>
-</div>
